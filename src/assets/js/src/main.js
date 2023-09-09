89cfc353343d7bc494850f45ffa6574785bce399
jenea130 <jenea130@mail.ru>
2023-09-09 10:45:16 +0300

    tried menu active: js

--- src/assets/js/src/main.js
+++ src/assets/js/src/main.js
@@ -2,7 +2,8 @@ import streetMap from "./modules/streetMap";
 import toggleMenu from "./modules/header/toggleMenu";
 
 window.addEventListener("DOMContentLoaded", function () {
-    console.log("some thing for me");
-    streetMap();
-    toggleMenu();
+	console.log("some thing for me");
+	streetMap();
+	toggleMenu();
 });
+
